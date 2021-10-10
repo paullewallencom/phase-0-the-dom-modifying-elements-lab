9d1885ca03093bf6f9a9cb853bd569dfaa3187d5
Paul Lewallen <paullewallencom@users.noreply.github.com>
2021-10-10 16:28:01 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+const element = document.getElementById('main');
+
+element.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = 'Paul is the champion';
+
+document.body.append(newHeader);
